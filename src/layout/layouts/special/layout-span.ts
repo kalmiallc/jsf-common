d91ca67e2140f271ac9eb51459fd5a83847d0cb7
Tilen Tomakić <tilen@tomakic.com>
2020-08-10 14:01:44 +0200

    JSF span info: icon.

--- src/layout/layouts/special/layout-span.ts
+++ src/layout/layouts/special/layout-span.ts
@@ -3,7 +3,9 @@ import { DefLayout, DefProp, DefExtends, DefCategory } from '../../../jsf-for-js
 import { DefLayoutInfo } from '../../../jsf-register-decorators';
 
 @DefLayoutInfo({
-  type: 'span'
+  type: 'span',
+  title: 'Span',
+  icon: 'layout-icons/span.svg'
 })
 @DefLayout({
   type : 'div',
