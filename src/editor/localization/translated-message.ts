1001d5fe7bda932a865becf2b05334e4d57e49ec
Matej Gorjanc <matej.gorjanc@kalmia.si>
2020-09-16 10:10:17 +0200

    Updated interface.

--- src/editor/localization/translated-message.ts
+++ src/editor/localization/translated-message.ts
@@ -1,9 +1,9 @@
 export interface TranslatedMessage {
 
-  sourceText: string;
-  sourceId: string;
+  sourceText?: string;
+  sourceId?: string;
 
-  targetText: string;
+  targetText?: string;
 
   /**
    * If set to true, this message is considered to be custom and we allow the user to modify the source as well as the target texts.
